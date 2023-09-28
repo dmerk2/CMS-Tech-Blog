d37ae427841c21c692a9681a0e399fe07bef6847
Dan Merkin <dan.merkin@gmail.com>
2023-09-28 10:36:26 -0400

    Correcting querySelector tag

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,13 @@
-const loginFormHandler = async (event) => {
+const loginHandler = async (event) => {
   event.preventDefault();
 
-  const email = document.querySelector("#email-login").value.trim();
+  const username = document.querySelector("#username-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
 
-  if (email && password) {
+  if (username && password) {
     const response = await fetch("/api/users/login", {
       method: "POST",
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ username: username, password: password }),
       headers: { "Content-Type": "application/json" },
     });
     if (response.ok) {
@@ -20,4 +20,4 @@ const loginFormHandler = async (event) => {
 
 document
   .querySelector(".login-form")
-  .addEventListener("submit", loginFormHandler);
+  .addEventListener("submit", loginHandler);
