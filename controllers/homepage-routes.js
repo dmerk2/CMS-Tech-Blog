59135f71a8da46615b6ee2356db720718e10c69f
Dan Merkin <dan.merkin@gmail.com>
2023-09-28 10:38:23 -0400

    Troubleshooting GET request for post id

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,11 +1,52 @@
 const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("layouts/main");
+    const posts = await Post.findAll({
+      include: [User],
+    });
+    const postData = posts.map((post) => post.get({ plain: true }));
+    res.render("homepage", { postData, user_id: req.session.user_id });
   } catch (error) {
     res.status(500).json(error);
   }
+});
+
+router.get("/post/:id", async (req, res) => {
+  try {
+    console.log("Route GET", req.params.id);
+    const onePost = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: {
+            exclude: "password",
+          },
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: {
+                exclude: "password",
+              },
+            },
+          ],
+        },
+      ],
+    });
+    const post = onePost.get({ plain: true });
+    console.log(post);
+    return res.render("single-post", { post, user_id: req.session.user_id });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 module.exports = router;
