b4c6bd79146871f261a72e0a6cfd6397e1dbfbbf
Dan Merkin <dan.merkin@gmail.com>
2023-09-28 10:35:40 -0400

    adding individualhooks for seeding users

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,31 +3,30 @@ const { User } = require("../models");
 const userData = [
   {
     username: "Test1",
-    email: "test1@gmail.com",
     password: "test1password",
   },
   {
     username: "Test2",
-    email: "test2@gmail.com",
     password: "test2password",
   },
   {
     username: "Test3",
-    email: "test3@gmail.com",
     password: "test3password",
   },
   {
     username: "Test4",
-    email: "test4@gmail.com",
     password: "test4password",
   },
   {
     username: "Test5",
-    email: "test5@gmail.com",
     password: "test5password",
   },
 ];
 
-const seedUsers = () => User.bulkCreate(userData);
+const seedUsers = () =>
+  User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
 
 module.exports = seedUsers;
