807df55ca402ff57e53dc75ad1011c38a6936e4b
Dan Merkin <dan.merkin@gmail.com>
2023-10-03 17:21:27 -0400

    Created format_date helper function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,12 +1,6 @@
 module.exports = {
   format_date: (date) => {
-    console.log("Input date:", date);
     let formattedDate = new Date(date);
-    console.log("Formatted date:", formattedDate); 
-    console.log("Month:", formattedDate.getMonth() + 1);
-    console.log("Day:", formattedDate.getDate());
-    console.log("Year:", formattedDate.getFullYear());
-
     return `${
       formattedDate.getMonth() + 1
     }/${formattedDate.getDate()}/${formattedDate.getFullYear()}`;
